513027d2d8b2a00af6e00483e12281addabcad93
GaynendraDev <gyanendragajpal88@gmail.com>
2025-11-08 19:34:53 +0530

    Fixed AuthLayout import and component name

--- src/AuthLayout.jsx
+++ src/AuthLayout.jsx
@@ -8,7 +8,7 @@ export default function Protected({children, authentication = true}) {
     const [loader, setLoader] = useState(true)
     const authStatus = useSelector(state => state.auth.status)
 
-    useEffect(() => {
+    useEffect(() => { 
         //TODO: make it more easy to understand
 
         // if (authStatus ===true){
